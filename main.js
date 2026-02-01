c013ccd9a54d4ca6f7c058a918038517d66db9a4
Kempanna Karevvagol <kempannakarevvagol@gmail.com>
2026-02-01 23:13:12 +0530

    Add personalized UI and implement add-task functionality

--- main.js
+++ main.js
@@ -1,20 +1,62 @@
-function openfeatures(){
-        let allElems = document.querySelectorAll(".elem")
-        let allfullElems = document.querySelectorAll(".fullElems")
-        let allfullElemsbackbtn = document.querySelectorAll(".backbtn")
-allElems.forEach(function(elems){
-        elems.addEventListener("click", function(){
-            allfullElems[elems.id].style.display = "block"
-        })       
-})
+function openfeatures() {
+  let allElems = document.querySelectorAll(".elem");
+  let allfullElems = document.querySelectorAll(".fullElems");
+  let allfullElemsbackbtn = document.querySelectorAll(".backbtn");
+  allElems.forEach(function (elems) {
+    elems.addEventListener("click", function () {
+      allfullElems[elems.id].style.display = "block";
+    });
+  });
 
-allfullElemsbackbtn.forEach(function(elems){
-        elems.addEventListener("click", function(){
-            allfullElems[elems.id].style.display = "none"
-           
-        })       
-})
+  allfullElemsbackbtn.forEach(function (elems) {
+    elems.addEventListener("click", function () {
+      allfullElems[elems.id].style.display = "none";
+    });
+  });
 }
-openfeatures()
+openfeatures();
+
+let form = document.querySelector(".addTask form");
+let taskInput = document.querySelector(".addTask form #task-input");
+let taskDetailsInput = document.querySelector(".addTask form textarea");
+let taskCheckBox = document.querySelector("#check");
 
 
+var currentTask = [];
+
+if(localStorage.getItem('currentTask')){
+        currentTask =JSON.parse(localStorage.getItem('currentTask'))
+        
+}
+else{
+        console.log("Task list is empty");   
+}
+
+function renderTask() {
+  let alltask = document.querySelector(".allTask");
+  let sum = "";
+  currentTask.forEach(function (elem) {
+    sum =
+      sum +
+      `<div class="task">
+                        <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+                        <button>Mark as Completed</button>
+                    </div>`;
+  });
+  alltask.innerHTML = sum;
+}
+renderTask()
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  currentTask.push({
+    task: taskInput.value,
+    details: taskDetailsInput.value,
+    imp: taskCheckBox.checked
+  });
+  localStorage.setItem('currentTask', JSON.stringify(currentTask))
+  taskInput.value = ''
+  taskDetailsInput.value = ''
+  taskCheckBox.checked = false
+  renderTask()
+});
